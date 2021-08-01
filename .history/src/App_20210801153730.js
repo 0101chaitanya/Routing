907e9c161ee8cbd7673eb6df9ab4c39d1e4cafc0
chaitanya <0101chaitanya@google.com>
2021-08-01 15:37:53 +0530

    fix

---
+++ .history/src/App_20210801153730.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Shop from "./components/Shop";
+import About from "./components/About";
+import ItemDetail from "./components/ItemDetail";
+import "./App.css";
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/shop/:id" component={ItemDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+const Home = () => (
+  <div>
+    <h1>Home page is me</h1>
+  </div>
+);
+
+export default App;
